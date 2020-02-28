5d853a0629cb54147826bfdbaf0e847f262dc39e
tnnmigga <tnnmigga@outlook.com>
2020-02-29 01:36:30 +0800

    修复代理

--- dl.py
+++ dl.py
@@ -18,8 +18,8 @@ if __name__=='__main__':
     args=get_cmd_args()
     if args[0]!=0:
         if args[1]=='open':
-            os.system("git config --global http.proxy 'socks5://127.0.0.1:1080'")
-            os.system("git config --global https.proxy 'socks5://127.0.0.1:1080'")
+            os.system("git config --global http.proxy socks5://127.0.0.1:1080")
+            os.system("git config --global https.proxy socks5://127.0.0.1:1080")
             print("开启了代理~")
         elif args[1]=='close':
             os.system("git config --global --unset http.proxy")
