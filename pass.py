db8853706f8e3427394806a18bf4ef355b4dad84
tnnmigga <tnnmigga@outlook.com>
2020-05-28 17:16:56 +0800

    更新代理

--- pass.py
+++ pass.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     args = get_cmd_args()
     if args[0] != 0:
         if args[1] == 'open':
-            os.system("git config --global http.proxy socks5://127.0.0.1:1080")
-            os.system("git config --global https.proxy socks5://127.0.0.1:1080")
+            os.system("git config --global http.proxy socks5://127.0.0.1:7891")
+            os.system("git config --global https.proxy socks5://127.0.0.1:7891")
             print("Open success")
         elif args[1] == 'close':
             os.system("git config --global --unset http.proxy")
